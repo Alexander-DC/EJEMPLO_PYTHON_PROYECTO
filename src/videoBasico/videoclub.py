95c026f38a4bb61b9a707f17a74e5a46b438ab64
Alexander <radezac@unac.edu.pe>
2020-04-08 01:26:04 -0500

    Se agrego clase Pelicula y sus clases

--- src/videoBasico/videoclub.py
+++ src/videoBasico/videoclub.py
@@ -9,3 +9,12 @@ class Socio:
        return 'DNI: {0}\nNombre: {1}\nTeléfono: {2}\nDomicilio: {3}\n'\
            .format(self.dni,self.nombre,self.telefono,self.domicilio)
     
+class Pelicula:
+    def __init__(self, titulo, genero):
+        self.titulo=titulo
+        self.genero=genero
+    
+    def __str__(self):
+        return 'Titulo: {0}\nGénero: {1}'.format(self.titulo,self.genero)
+
+
