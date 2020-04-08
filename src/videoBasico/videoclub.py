8286455304359b51201d7679233aad2f4ebc7eab
Alexander <radezac@unac.edu.pe>
2020-04-07 19:46:49 -0500

    Agregue Clases init str

--- src/videoBasico/videoclub.py
+++ src/videoBasico/videoclub.py
@@ -1,1 +1,11 @@
-hola
+class Socio:
+    def __init__(self, dni, nombre, telefono, domicilio):
+        self.dni=dni
+        self.nombre=nombre
+        self.telefono=telefono
+        self.domicilio=domicilio
+        
+    def __str__(self):
+       return 'DNI: {0}\nNombre: {1}\nTeléfono: {2}\nDomicilio: {3}\n'\
+           .format(self.dni,self.nombre,self.telefono,self.domicilio)
+    
